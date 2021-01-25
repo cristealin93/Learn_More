7493dbf709daae3bf58837797bbf08cec7a23b26
cristealin93 <cristealin93@gmail.com>
2021-01-25 20:40:38 +0200

    First day update: 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
+import javax.swing.text.html.parser.Parser;
+import java.sql.Array;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String [] args){
 
+        Scanner sc =new Scanner(System.in);
+
+        int a=sc.nextInt();
+
+        int []val=new int[a];
+
+        for(int i=0;i<a;i++){
+            String c=sc.next();
+            val[i]= Integer.parseInt(c);
+        }
+       test(val);
         System.out.println("Tuc");
+
+    }
+    public static void test(int arr[]){
+
+        HashMap<Integer,Integer> hashMap=new HashMap<Integer,Integer>();
+
+        int n= arr.length;
+        for(int i=0;i<n;i++){
+            if(hashMap.containsKey(arr[i])){
+                hashMap.put(arr[i], hashMap.get(arr[i])+1);
+            }else{
+                hashMap.put(arr[i],1);
+            }
+        }
+        for(int c:hashMap.keySet()){
+            if(hashMap.get(c)/2>=1){
+                System.out.println( "Pentru key "+c+" Valoare este "+hashMap.get(c)/2);
+            }
+
+        }
+
+
     }
 }
